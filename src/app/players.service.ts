67676312759130a3ca666accace1b183161e7f87
babysharny <babysharny@gmail.com>
2016-08-10 22:03:13 +0300

    commit

--- src/app/players.service.ts
+++ src/app/players.service.ts
@@ -13,7 +13,9 @@ export class PlayersService {
   }
 
   startGame(game) {
-    let url = `http://localhost:8080/1/employees/${game.appID}?secret_admin=boilerplatesRock`;
+    let host = '192.168.1.70:8080';
+    // let host = 'localhost:8080';
+    let url = `http://${host}/1/employees/${game.appID}?secret_admin=boilerplatesRock`;
     // let url = `http://localhost:8080/1/employees`;
     console.info('START GAME ', game);
     console.info(url);
